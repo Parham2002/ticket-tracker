db478ef148ec7bc7db9464d93f480f944d284b28
Parham2002 <parhamm2002@gmail.com>
2022-09-07 12:49:24 +0100

    added the counter number in counter container

---
+++ src/components/Main/Card/CounterContainer/Counter/Counter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Counter.scss"
+
+const Counter = () => {
+  return (
+    <div className='counter'>0</div>
+  )
+}
+
+export default Counter
--- src/components/Main/Card/CounterContainer/CounterContainer.jsx
+++ src/components/Main/Card/CounterContainer/CounterContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./CounterContainer.scss"
+import Counter from "./Counter/Counter"
 
 const CounterContainer = () => {
   return (
-    <div className='counter-container'></div>
+    <div className='counter-container'>
+      Counter
+      <Counter/>
+    </div>
   )
 }
 

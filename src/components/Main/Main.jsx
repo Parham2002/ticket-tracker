6263d80c234215cd3afc25abb0ee7bf7723998a9
Parham2002 <parhamm2002@gmail.com>
2022-09-09 16:01:40 +0100

    fixed background, added some pseudocode

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
 import "./Main.scss";
 import Card from './Card/Card.jsx';
+// Fetching data to use for the cards
 import teamData from '../../assets/data/teamData';
 import plusSign from "../../assets/images/plus-sign.png"
 
 function Main() {
+  // useState for adding new cards on click
   const [newCard, addNewCard] = useState()
 
   const handleCards = () => {
     addNewCard(teamData.push({
       id: teamData.length+1,
-      name: "NPC X",
+      name: "NPC #" + (teamData.length+1),
       role: "Software Developer"
     }))
   }
   
   return (
+    // Main div responsible for generating and displaying the cards
     <div className='main-container'>
       {teamData.map((employee)=>{
+        // Passing teamData as props to the card component
         return (<Card key={employee.id} data={employee}/>)
       })}
       <div className='main-container__addcard'>
